30fdeb96305ed27418b6e98c291e6a683dab18a7
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-11-04 12:21:04 -0500

    Laid foundations of SearchResultsPage.js

---
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+let defaultSearch = {
+    q: '',
+}
+
+const SearchResultsPage = () => {
+
+}
+
+export default SearchResultsPage
