219b9fb1271c77e8e42cc2a2e2a97725cb2b9094
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-11-09 23:06:03 -0600

    Imported iFrame Player into PublicYoutubePage.js

--- frontend/src/pages/PublicYoutubePage/PublicYoutubePage.js
+++ frontend/src/pages/PublicYoutubePage/PublicYoutubePage.js
@@ -3,7 +3,15 @@ import React from 'react';
 const PublicYoutubePage = ({currentVideo}) => {
     return (
         <div>
-            {currentVideo.snippet.title}
+            <iframe
+                title='default-player'
+                id='player'
+                type='text/html'
+                width='640'
+                height='390'
+                src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                frameBorder='0'
+            ></iframe>
         </div>
     )
 
