fcd8740cea1aeccbe2a3beb31b7907d087567358
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-11-22 21:24:15 -0600

    Cleaned up VideoPage.js

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -1,37 +1,17 @@
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import { API_KEY } from "../../API_KEYS/API_KEY1";
-import VideoMapper from "../../components/VideoMapper/VideoMapper";
+import { useNavigate, useParams } from "react-router-dom";
 
-const VideoPage = ({video, setSearchResults, searchResults}) => {
+const VideoPage = () => {
     const {videoId} = useParams();
 
-    const GetRelatedResults = async () => {
-        try {
-            let response = await axios.get(
-              `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&relatedToVideoId=${videoId}&type=video&maxResults=5`
-            );
-            setSearchResults(response.data.items);
-          } catch (error) {
-            console.log(error.response.data);
-          }
+    let navigate = useNavigate();
+    const goToSearchPage = () => {
+      let path = '/searchpage';
+      navigate(path);
     }
 
     return (
         <div>
-          <button onClick={()=>{GetRelatedResults()}}>Click for Related</button>
-            <iframe
-                title= "default-player"
-                id= "player"
-                type= "text/html"
-                width= "640"
-                height= "390"
-                src= {`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com`}
-                frameBorder='0'
-            ></iframe>
-            <h2>{videoId}</h2>
-            <VideoMapper searchResults={searchResults} />
-
+          <button onClick={goToSearchPage}>Go to Search Page</button>
         </div>
     )
 }
