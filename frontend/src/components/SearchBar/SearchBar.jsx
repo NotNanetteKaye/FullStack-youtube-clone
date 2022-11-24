356d2d79dad67caca59721f418c638c8619fe23d
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-11-23 23:45:26 -0600

    Updated SearchResultsPage.js and SearchBar.jsx

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,31 +1,20 @@
 import './SearchBar.css'
 
-const SearchBar = ({inputHandler, GetResults, inputText}) => {
+const SearchBar = ({getVideos}) => {
+
+  let inputHandler = (e) => {
+    const lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
 
     return(
         <div className='search'>
             <div className='searchInputs'>
                 <input type='text' value={inputText} placeholder='Search for videos here...' onChange={inputHandler}/>
-                <button onClick={() => {GetResults()}}>Search here!</button>
+                <button onClick={() => {getVideos()}}>Search here!</button>
             </div>
         </div>
     )
 }
 
 export default SearchBar
-
-let inputHandler = (e) => {
-    const lowerCase = e.target.value.toLowerCase();
-    setInputText(lowerCase);
-  };
-
-  const GetResults = async () => {
-    try {
-      let response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${inputText}&part=snippet&maxResults=10`
-      );
-      setSearchResults(response.data.items);
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
--- frontend/src/pages/SearchResultsPage/SearchResultsPage.js
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,5 +1,53 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import useAuth from '../../hooks/useAuth';
+import VideoMapper from '../../components/VideoMapper/VideoMapper';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import {API_KEY} from './API_KEYS/API_KEY1'
 
 
-const SearchResultsPage = () => {};
+const SearchResultsPage = () => {
+
+    const [user, token] = useAuth();
+    const [videos, setVideos] = useState([])
+
+    const getVideos = async (videoTitle = 'recess therapy') => {
+        try{
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${videoTitle}&part=snippet&maxResults=10`
+            );
+            setVideos(response.data.items);
+            console.log(response.data)
+        } catch (error){
+            console.log(error.response.data);
+        }
+        };
+
+    useEffect(() => {    
+        getVideos();
+    }, [token]);
+
+
+    const getVideo = (videoId) => {
+        
+    }
+
+
+    return (
+        <div>
+            <SearchBar getVideos = {getVideos} />
+            <div>
+            {videos.map((video, index) => {
+                return(
+                <Link to={`/videopage/${video.id.videoId}`}>
+                    <div className='column' key = {index}>{video.snippet ?  <img className = 'thumbnail' src={video.snippet.thumbnails.default.url} onClick={ () => getVideo(video)}></img> : null}    
+                    </div>
+                </Link>
+                )
+            })}    
+            </div>
+
+        </div>
+    )
+};
 
 export default SearchResultsPage;
