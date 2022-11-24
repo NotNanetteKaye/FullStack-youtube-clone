29589b58d251b3935e4ae1a618932b63de0f913a
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-11-23 21:01:54 -0600

    Logic for GetRelatedVideos.jsx inserted

--- frontend/src/components/GetRelatedVideos/GetRelatedVideos.jsx
+++ frontend/src/components/GetRelatedVideos/GetRelatedVideos.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import axios from 'axios';
 
 const GetRelatedVideos = () => {
+
+    const [relatedVideos, setRelatedVideos] = useState([]);
+
+    const GetRelatedResults = async () => {
+        try {
+            let response = await axios.get(
+              `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&relatedToVideoId=${videoId}&type=video&maxResults=5`
+            );
+            setRelatedVideos(response.data.items);
+          } catch (error) {
+            console.log(error.response.data);
+          }
+    }
+
     return (
         <div>
 
