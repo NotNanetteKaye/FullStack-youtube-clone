effca4f2fd639a48779fe7d3b330294049fbf797
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-11-24 19:30:50 -0600

    Issues with CreateComment.jsx

--- frontend/src/components/CreateComment/CreateComment.jsx
+++ frontend/src/components/CreateComment/CreateComment.jsx
@@ -1,43 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import useAuth from '../../hooks/useAuth';
-const url = 'http://127.0.0.1:8000/api/comments/'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
 
-const CreateComment = ({videoID}) => {
-    const [comment, setComment] = useState('')
-    const [user, token] = useAuth()
+const CreateComment = ({ videoID }) => {
+  const [comment, setComment] = useState("");
+  const [user, token] = useAuth();
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        let newComment = {
-            video_id: videoID,
-            text: comment,
-            likes: 0,
-            dislikes: 0,
-            user_id: user.id
-        };
-        try {
-            const response = await axios.post(url, newComment, {headers: {Authorization: "Bearer" + token}});
-            console.log(newComment);
-        } catch (error) {
-            console.log(error.response);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let newComment = {
+      video_id: { videoID },
+      text: comment,
+      likes: 0,
+      dislikes: 0,
+      user_id: user.id,
+    };
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/comments/",
+        newComment,
+        {
+          headers: { Authorization: "Bearer" + token },
         }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
     }
+  };
 
-    return(
-       <div>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <input type='text' value={comment} onChange={(e) => setComment(e.target.value)}/>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={() => response()}>
+            Submit Comment
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-            </div>
-            <div>
-                <button type='submit'>Submit Comment</button>
-            </div>
-        </form>
-       </div> 
-    )
-}
-
-export default CreateComment
+export default CreateComment;
